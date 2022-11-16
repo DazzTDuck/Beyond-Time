7358effba105340c6749cbfb88f7965a83158333
DazzT <yerio2003@gmail.com>
2022-11-16 14:35:47 +0100

    Project commit

---
+++ BeyondTimeProject/Source/BeyondTimeProject/BeyondTimeProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BeyondTimeProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BEYONDTIMEPROJECT_API ABeyondTimeProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
